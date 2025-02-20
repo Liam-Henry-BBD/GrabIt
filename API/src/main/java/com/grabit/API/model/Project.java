1f1ebc2a65a9288f1cc88348cf15527b91a7df1e
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-02-20 14:23:21 +0200

    added Project repository and updated ID type to integer

--- API/src/main/java/com/grabit/API/model/Project.java
+++ API/src/main/java/com/grabit/API/model/Project.java
@@ -4,15 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@NoArgsConstructor
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ProjectId;
+    private Integer ProjectId;
     @Getter
     @Setter
     private String ProjectName;
--- API/src/main/java/com/grabit/API/repository/ProjectRepository.java
+++ API/src/main/java/com/grabit/API/repository/ProjectRepository.java
@@ -1,5 +1,9 @@
 package com.grabit.API.repository;
 
-public class ProjectRepository {
-    
+import com.grabit.API.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
 }
