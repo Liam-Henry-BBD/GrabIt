4048162c2491e354b8876a4fd70654d9702fa491
Tadima Monama <tadima.monama@bbd.co.za>
2025-02-20 14:57:05 +0200

    task status

---
+++ API/src/main/java/com/grabit/API/model/TaskStatus.java
@@ -0,0 +1,26 @@
+package com.grabit.API.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TaskStatus", schema = "grabit")
+public class TaskStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TaskStatusID", nullable = false)
+    private byte taskStatusID;
+
+    @Column(name = "StatusName", nullable = false, length = 50)
+    private String statusName;
+
+}
