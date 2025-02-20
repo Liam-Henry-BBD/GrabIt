898e505410076a49f4bb3dc5ce704bf0032c1f94
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-02-20 13:48:38 +0200

    added project model

--- API/src/main/java/com/grabit/API/model/Project.java
+++ API/src/main/java/com/grabit/API/model/Project.java
@@ -1,5 +1,28 @@
 package com.grabit.API.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Date;
+
+@Entity
 public class Project {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ProjectId;
+    @Getter
+    @Setter
+    private String ProjectName;
+    @Getter
+    @Setter
+    private String ProjectDescription;
+    @Getter
+    @Setter
+    private Date CreatedAt;
+    @Getter
+    @Setter
+    private Date UpdatedAt;
 }
