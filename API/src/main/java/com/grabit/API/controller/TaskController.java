0deb65f2e06c05968e633b397e26e43d76900d7a
Tadima Monama <tadima.monama@bbd.co.za>
2025-02-20 14:55:08 +0200

    Task controller

---
+++ API/src/main/java/com/grabit/API/controller/TaskController.java
@@ -0,0 +1,52 @@
+package com.grabit.API.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grabit.API.model.Task;
+import com.grabit.API.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return taskService.getTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Integer id) {
+        return taskService.getTaskById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Integer id) {
+        taskService.deleteTask(id);
+    }
+
+    @PostMapping
+    public Task createTask(Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Integer id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+}
