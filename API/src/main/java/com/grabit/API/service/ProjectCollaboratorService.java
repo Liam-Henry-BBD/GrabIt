b645b7c58908814bba4580435abf1c97e7dfc9d3
Tshilidzi Munyai <Tshilidzi.Munyai@bbd.co.za>
2025-02-20 14:22:13 +0200

    Add ProjectCollaboratorService with CRUD operations

--- API/src/main/java/com/grabit/API/service/ProjectCollaboratorService.java
+++ API/src/main/java/com/grabit/API/service/ProjectCollaboratorService.java
@@ -1,7 +1,36 @@
-// implement the business logic of your application
-// interact with the repository layer to perform CRUD operations and other business-related tasks
 package com.grabit.API.service;
 
+import com.grabit.API.model.ProjectCollaborator;
+import com.grabit.API.repository.ProjectCollaboratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProjectCollaboratorService {
-    
+
+    @Autowired
+    private ProjectCollaboratorRepository projectCollaboratorRepository;
+
+    public List<ProjectCollaborator> getAllProjectCollaborators() {
+        return projectCollaboratorRepository.findAll();
+    }
+
+    public ProjectCollaborator addProjectCollaborator(ProjectCollaborator projectCollaborator) {
+        return projectCollaboratorRepository.save(projectCollaborator);
+    }
+
+    public ProjectCollaborator getProjectCollaboratorByID(Long id) {
+        return projectCollaboratorRepository.findById(id).orElse(null);
+    }
+
+    public ProjectCollaborator updateProjectCollaborator(Long id, ProjectCollaborator projectCollaborator) {
+        projectCollaborator.setProjectCollaboratorID(id);
+        return projectCollaboratorRepository.save(projectCollaborator);
+    }
+
+    public void deleteProjectCollaborator(Long id) {
+        projectCollaboratorRepository.deleteById(id);
+    }
 }
