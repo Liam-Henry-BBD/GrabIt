573b0177b2cb98e91f95e08fb72aff518c791fde
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-02-23 19:05:07 +0200

    added leaderboard end point and logic

--- API/src/main/java/com/grabit/API/controller/ProjectController.java
+++ API/src/main/java/com/grabit/API/controller/ProjectController.java
@@ -1,15 +1,14 @@
 package com.grabit.API.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.grabit.API.dto.ProjectLeaderboardDTO;
 import com.grabit.API.model.Project;
 import com.grabit.API.model.ProjectCollaboratorModel;
 import com.grabit.API.model.Task;
-import com.grabit.API.model.TaskCollaborator;
 import com.grabit.API.service.ProjectService;
 
 import java.util.List;
@@ -26,47 +25,43 @@ public class ProjectController {
     }
 
     // Create or update a project
-//    @PostMapping
-//    public ResponseEntity<Project> createProject(@RequestBody Project project) {
-//        Project savedProject = projectService.saveProject(project);
-//        return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
-//    }
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        Project savedProject = projectService.saveProject(project);
+        return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+    }
 
     // Get all projects
-//    @GetMapping
-//    public ResponseEntity<List<Project>> getAllProjects() {
-//        return ResponseEntity.ok(projectService.getAllProjects());
-//    }
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllProjects() {
+        return ResponseEntity.ok(projectService.getAllProjects());
+    }
 
     // Get a project by its ID
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Project> getProjectById(@PathVariable Integer id) {
-//        return ResponseEntity.ok(projectService.getProjectById(id));
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Project> getProjectById(@PathVariable Integer id) {
+        return ResponseEntity.ok(projectService.getProjectById(id));
+    }
 
     // close a project by its ID
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> closeProject(@PathVariable Integer id) {
-//        projectService.closeProject(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> closeProject(@PathVariable Integer id) {
+        projectService.closeProject(id);
+        return ResponseEntity.noContent().build();
+    }
 
-//    @GetMapping("/{id}/tasks")
-//    public List<Task> getProjectTasks(@PathVariable Integer id) {
-//        return projectService.getProjectTasksByProjectId(id);
-//    }
+    @GetMapping("/{id}/tasks")
+    public List<Task> getProjectTasks(@PathVariable Integer id) {
+        return projectService.getProjectTasksByProjectId(id);
+    }
 
-//    @GetMapping("/{id}/collaborators")
-//    public ResponseEntity<List<ProjectCollaboratorModel>> getProjectCollaborators(@PathVariable Integer id) {
-//        return ResponseEntity.ok(projectService.getProjectCollaboratorsByProjectId(id));
-//    }
+    @GetMapping("/{id}/collaborators")
+    public ResponseEntity<List<ProjectCollaboratorModel>> getProjectCollaborators(@PathVariable Integer id) {
+        return ResponseEntity.ok(projectService.getProjectCollaboratorsByProjectId(id));
+    }
 
-    // @GetMapping("/{id}/leaderboard")
-    // public ResponseEntity<List<TaskCollaborator>> getProjectLeaderboard(@PathVariable Integer id) {
-    //     return ResponseEntity.ok(projectService.getProjectLeaderboardByProjectId(id));
-    // }
-
-    // public List<TaskCollaborator> getProjectLeaderboard(@PathVariable Integer id) {
-    //     return projectService.getProjectLeaderboardByProjectId(id);
-    // }
+    @GetMapping("/{id}/leaderboard")
+    public ResponseEntity<List<ProjectLeaderboardDTO>> getProjectLeaderboard(@PathVariable Integer id) {
+        return ResponseEntity.ok(projectService.getProjectLeaderboardByProjectId(id));
+    }
 }
--- API/src/main/java/com/grabit/API/service/ProjectService.java
+++ API/src/main/java/com/grabit/API/service/ProjectService.java
@@ -3,15 +3,11 @@ package com.grabit.API.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.grabit.API.dto.ProjectLeaderboardDTO;
 import com.grabit.API.model.Project;
 import com.grabit.API.model.ProjectCollaboratorModel;
 import com.grabit.API.model.Task;
-//<<<<<<< Updated upstream
-//import com.grabit.API.model.TaskCollaborators;
-//import com.grabit.API.repository.ProjectCollaboratorRepository;
-//=======
-import com.grabit.API.model.TaskCollaborator;
-//>>>>>>> Stashed changes
+import com.grabit.API.repository.ProjectCollaboratorRepository;
 import com.grabit.API.repository.ProjectRepository;
 import com.grabit.API.repository.TaskCollaboratorRepository;
 import com.grabit.API.repository.TaskRepository;
@@ -23,77 +19,63 @@ import java.util.stream.Collectors;
 public class ProjectService extends Task {
 
     // private TaskRepository taskRepository;
-//    private final ProjectRepository projectRepository;
-//    private final TaskRepository taskRepository;
-//<<<<<<< Updated upstream
-//    private final TaskCollaboratorsRepository taskCollaboratorsRepository;
-//    private final ProjectCollaboratorRepository projectCollaboratorRepository;
+    private final ProjectRepository projectRepository;
+    private final TaskRepository taskRepository;
+    private final ProjectCollaboratorRepository projectCollaboratorRepository;
 
-//    @Autowired
-//    public ProjectService(ProjectRepository projectRepository, TaskRepository taskRepository,
-//            TaskCollaboratorsRepository taskCollaboratorsRepository,
-//            ProjectCollaboratorRepository projectCollaboratorRepository) {
-//        this.projectRepository = projectRepository;
-//        this.taskRepository = taskRepository;
-//        this.taskCollaboratorsRepository = taskCollaboratorsRepository;
-//        this.projectCollaboratorRepository = projectCollaboratorRepository;
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository, TaskRepository taskRepository,
+            TaskCollaboratorRepository taskCollaboratorsRepository,
+            ProjectCollaboratorRepository projectCollaboratorRepository) {
+        this.projectRepository = projectRepository;
+        this.taskRepository = taskRepository;
+        this.projectCollaboratorRepository = projectCollaboratorRepository;
 
-//=======
-//    private final TaskCollaboratorRepository taskCollaboratorRepository;
-//
-//    @Autowired
-//    public ProjectService(ProjectRepository projectRepository, TaskRepository taskRepository,
-//            TaskCollaboratorRepository taskCollaboratorRepository) {
-//        this.projectRepository = projectRepository;
-//        this.taskRepository = taskRepository;
-//        this.taskCollaboratorRepository = taskCollaboratorRepository;
-//>>>>>>> Stashed changes
-//    }
+    }
 
     // Save a new project or update an existing one
-//    public Project saveProject(Project project) {
-//        return projectRepository.save(project);
-//    }
+    public Project saveProject(Project project) {
+        return projectRepository.save(project);
+    }
 
     // Get all projects
-//    public List<Project> getAllProjects() {
-//        return projectRepository.findAll();
-//    }
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
 
     // Get a project by its ID
-//    public Project getProjectById(Integer id) {
-//        return projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Project not found"));
-//    }
+    public Project getProjectById(Integer id) {
+        return projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Project not found"));
+    }
 
     // Delete a project by ID
-//    public void closeProject(Integer id) {
-//        projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Project not found"));
-//        projectRepository.deleteById(id);
-//    }
-//
-//    public List<Task> getProjectTasksByProjectId(Integer projectID) {
-//        return taskRepository.findByProject_ProjectID(projectID);
-//    }
+    public void closeProject(Integer id) {
+        projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Project not found"));
+        projectRepository.deleteById(id);
+    }
 
-//    public List<TaskCollaborator> getProjectLeaderboardByProjectId(Integer projectID) {
-//        List<Task> tasks = taskRepository.findByProject_ProjectID(projectID);
-//<<<<<<< Updated upstream
-//        List<Task> completedTasks = tasks.stream().filter(task -> task.getTaskStatus().getTaskStatusID() == 2)
-//                .collect(Collectors.toList());
-//        List<TaskCollaborators> taskCollaborators = completedTasks.stream()
-//                .flatMap(task -> taskCollaboratorsRepository.findById(task.getTaskID()).stream())
-//                .collect(Collectors.toList());
-//        // List<Object>
-//=======
-//        List<Task> completedTasks = tasks.stream().filter(task -> task.getTaskStatus().getTaskStatusID() == 2).collect(Collectors.toList());
-//        List<TaskCollaborator> taskCollaborator = completedTasks.stream().flatMap(task -> taskCollaboratorRepository.findById(task.getTaskID()).stream()).collect(Collectors.toList());
-//        // List<Object>
-//>>>>>>> Stashed changes
+    public List<Task> getProjectTasksByProjectId(Integer projectID) {
+        return taskRepository.findByProject_ProjectID(projectID);
+    }
 
-//        return taskCollaborator;
-//    }
+    public List<ProjectLeaderboardDTO> getProjectLeaderboardByProjectId(Integer projectId) {
+        List<Object[]> results = projectRepository.getProjectLeaderboard(projectId);
 
-//    public List<ProjectCollaboratorModel> getProjectCollaboratorsByProjectId(Integer projectID) {
-//        return projectCollaboratorRepository.findByProject_ProjectID(projectID);
-//    }
+        // Convert List<Object[]> to DTO and sort by TotalScore (Descending)
+        List<ProjectLeaderboardDTO> leaderboard = results.stream()
+                .map(row -> new ProjectLeaderboardDTO(null, (Integer) row[0], (String) row[1], (Integer) row[2]))
+                .sorted((a, b) -> b.getTotalScore().compareTo(a.getTotalScore()))
+                .collect(Collectors.toList());
+
+        // Assign positions (1-based index)
+        for (int i = 0; i < leaderboard.size(); i++) {
+            leaderboard.get(i).setPosition(i + 1);
+        }
+
+        return leaderboard;
+    }
+
+    public List<ProjectCollaboratorModel> getProjectCollaboratorsByProjectId(Integer projectID) {
+        return projectCollaboratorRepository.findByProject_ProjectID(projectID);
+    }
 }
