7a08b6880cd63c2b9c3b665957b5e58edd1b154f
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-02-25 14:12:06 +0200

    modified leaderboard query

--- API/src/main/java/com/grabit/API/repository/ProjectRepository.java
+++ API/src/main/java/com/grabit/API/repository/ProjectRepository.java
@@ -12,18 +12,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     @Query(value = """
-            SELECT u.UserID, u.GitHubID, SUM(COALESCE(tp.TaskPointID, 0)) AS TotalScore
-            FROM (SELECT DISTINCT tc.UserID, tc.TaskID
-                  FROM [grabit].[TaskCollaborators] tc
-                  JOIN [grabit].[Tasks] t ON t.TaskID = tc.TaskID
-                  WHERE tc.isActive = 1) AS distinct_user_tasks
-            JOIN [grabit].[Users] u ON u.UserID = distinct_user_tasks.UserID
-            JOIN [grabit].[Tasks] t ON t.TaskID = distinct_user_tasks.TaskID
-            JOIN [grabit].[TaskPoints] tp ON tp.TaskPointID = t.TaskPointID
-            WHERE t.TaskStatusID = 4 AND t.ProjectID = :projectID
-            GROUP BY u.UserID, u.GitHubID
-            ORDER BY TotalScore DESC
-            """, nativeQuery = true)
+            SELECT
+                u.UserID,
+                u.GitHubID,
+                SUM(COALESCE(tp.TaskPointID, 0)) AS TotalScore
+            FROM
+                [grabit].[Users] u
+            JOIN
+                [grabit].[TaskCollaborators] tc ON u.UserID = tc.UserID
+            JOIN
+                [grabit].[Tasks] t ON t.TaskID = tc.TaskID
+            LEFT JOIN
+                [grabit].[TaskPoints] tp ON tp.TaskPointID = t.TaskPointID
+            WHERE
+                tc.isActive = 1
+                AND t.TaskStatusID = 4
+                AND t.ProjectID = :projectID
+            GROUP BY
+                u.UserID, u.GitHubID
+            ORDER BY
+                TotalScore DESC;
+                        """, nativeQuery = true)
     List<Object[]> getProjectLeaderboard(@Param("projectID") Integer projectID);
 
 }
