d0bb3d07f545ad3a34cd04b9b9a4aacbedbbcd69
Tadima Monama <tadima.monama@bbd.co.za>
2025-02-27 06:51:25 +0200

    adding custom methods that do not use snake casing

--- API/src/main/java/com/grabit/app/repository/TaskCollaboratorRepository.java
+++ API/src/main/java/com/grabit/app/repository/TaskCollaboratorRepository.java
@@ -1,6 +1,8 @@
 package com.grabit.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.grabit.app.model.TaskCollaborator;
@@ -9,5 +11,7 @@ import java.util.List;
 
 @Repository
 public interface TaskCollaboratorRepository extends JpaRepository<TaskCollaborator, Integer> {
-    List<TaskCollaborator> findByTask_TaskID(Integer taskID);
+
+    @Query(value = "SELECT * FROM TaskCollaborators task WHERE TaskID = :taskID", nativeQuery = true)
+    List<TaskCollaborator> findByTaskID(@Param("taskID") Integer taskID);
 }
--- API/src/main/java/com/grabit/app/repository/UserRepository.java
+++ API/src/main/java/com/grabit/app/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.grabit.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.grabit.app.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "SELECT * FROM Users WHERE GitHubID = :githubID", nativeQuery = true)
+    User findByGitHubId(String githubID);
 }
