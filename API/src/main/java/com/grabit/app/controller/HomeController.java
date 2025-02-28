ac8305c63a279b25b17ad4d9cc62903b9b1feadd
Tadima Monama <tadima.monama@bbd.co.za>
2025-02-28 19:09:04 +0200

    send a token back to the client after successfully login using the provider

--- API/src/main/java/com/grabit/app/controller/HomeController.java
+++ API/src/main/java/com/grabit/app/controller/HomeController.java
@@ -1,5 +1,11 @@
 package com.grabit.app.controller;
 
+import com.grabit.app.dto.TokenDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +19,29 @@ import java.util.Map;
 @RequestMapping("/")
 public class HomeController {
 
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public HomeController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
     @GetMapping
+    public ResponseEntity<TokenDTO> home(@AuthenticationPrincipal OAuth2User principal,
+                                         OAuth2AuthenticationToken authenticationToken) {
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+                authenticationToken.getAuthorizedClientRegistrationId(),
+                authenticationToken.getName());
+
+        if(principal != null) {
+            if (client != null && client.getAccessToken() != null) {
+                return ResponseEntity.ok(new TokenDTO(client.getAccessToken().getTokenValue()));
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    //TODO: We need Swagger for documentation
+    @GetMapping("/docs")
     public String welcome() {
         return """
                     <!DOCTYPE html>
