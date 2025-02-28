ab0c198c072a543eee9b04a21fc5862079331f83
Tshilidzi Munyai <Tshilidzi.Munyai@bbd.co.za>
2025-02-28 10:31:35 +0200

    adding service to githubservice

--- API/src/main/java/com/grabit/app/service/GitHubService.java
+++ API/src/main/java/com/grabit/app/service/GitHubService.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
+@Service
 public class GitHubService {
     public String getGitHubUserLogin(String token) {
         RestTemplate restTemplate = new RestTemplate();
