53e8593de6a6ea32fe434bffb12681772f608bb4
Tadima Monama <tadima.monama@bbd.co.za>
2025-03-01 09:25:13 +0200

    making exception handling more robust

--- API/src/main/java/com/grabit/app/exceptions/ExceptionResolver.java
+++ API/src/main/java/com/grabit/app/exceptions/ExceptionResolver.java
@@ -2,25 +2,35 @@ package com.grabit.app.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionResolver {
 
     @ExceptionHandler(NotFound.class)
-    public ResponseEntity<?> handleNotFound(NotFound ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Error> handleNotFound(NotFound ex) {
+        Error error = new Error(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadRequest.class)
-    public ResponseEntity<?> handleBadRequest(BadRequest ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Error> handleBadRequest(BadRequest ex) {
+        Error error = new Error(ex.getReason(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Unauthorized.class)
-    public ResponseEntity<?> handleUnauthorized(Unauthorized ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Error> handleUnauthorized(Unauthorized ex) {
+        Error error = new Error(ex.getReason(), HttpStatus.UNAUTHORIZED.value());
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Error> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Error error = new Error("Validation failed.", HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
