28a649112f6f723ebe664ad02f25e2aa5f8a7b7b
Tadima Monama <tadima.monama@bbd.co.za>
2025-03-03 09:50:23 +0200

    Introducing enums for more readability and type safety

--- API/src/main/java/com/grabit/app/enums/Roles.java
+++ API/src/main/java/com/grabit/app/enums/Roles.java
@@ -1,4 +1,16 @@
 package com.grabit.app.enums;
 
-public class Roles {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Roles {
+    PROJECT_LEAD((byte) 1),
+    PROJECT_MEMBER((byte) 2),
+    TASK_GRABBER((byte) 3),
+    TASK_COLLABORATOR((byte) 4);
+
+    private final byte role;
+
 }
