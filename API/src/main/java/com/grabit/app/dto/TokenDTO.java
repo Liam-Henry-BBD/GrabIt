e4fbcc2b436c0550b6797bfb5276f8fe270164d8
Siphesihle Mnyengeza <Siphesihle.Mnyengeza@bbd.co.za>
2025-03-07 10:54:04 +0200

    removed redundant imports

--- API/src/main/java/com/grabit/app/dto/TokenDTO.java
+++ API/src/main/java/com/grabit/app/dto/TokenDTO.java
@@ -3,8 +3,6 @@ package com.grabit.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfToken;
 
 @Getter
 @Setter
