5a403c87695bf9b8a1024e0453f6cccb2ecfb745
Tadima Monama <tadima.monama@bbd.co.za>
2025-03-07 09:39:46 +0200

    token dto

--- API/src/main/java/com/grabit/app/dto/TokenDTO.java
+++ API/src/main/java/com/grabit/app/dto/TokenDTO.java
@@ -4,10 +4,12 @@ package com.grabit.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.web.csrf.CsrfToken;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class TokenDTO {
     private String token;
+    private String csrf;
 }
