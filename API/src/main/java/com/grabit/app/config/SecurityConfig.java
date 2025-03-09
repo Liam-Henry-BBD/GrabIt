88bbfb2a8296a88c006f3ea896710819d8b71bca
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-03-09 13:07:56 +0200

    enhance security configuration to require authentication for specific endpoints and improve CSRF protection

--- API/src/main/java/com/grabit/app/config/SecurityConfig.java
+++ API/src/main/java/com/grabit/app/config/SecurityConfig.java
@@ -5,13 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -25,11 +20,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .addFilterBefore(authFilter, BasicAuthenticationFilter.class)
-                .oauth2Login(Customizer.withDefaults());
-
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/login/**", "/oauth2/**", "/favicon.ico", "/").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/login/**", "/oauth2/**", "/favicon.ico", "/")
+                        .requireCsrfProtectionMatcher(request -> {
+                            String authorizationHeader = request.getHeader("Authorization");
+                            return !(authorizationHeader != null && authorizationHeader.startsWith("Bearer "));
+                        }));
         return http.build();
     }
+
 }
