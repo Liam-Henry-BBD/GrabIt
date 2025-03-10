c21d0788306821b82e9278895f21d046e6032675
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-03-10 15:57:33 +0200

    fixed taskDTO types

--- API/src/main/java/com/grabit/app/dto/TaskDTO.java
+++ API/src/main/java/com/grabit/app/dto/TaskDTO.java
@@ -1,20 +1,23 @@
 package com.grabit.app.dto;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDTO {
+    private Integer taskID;
     private String taskName;
     private String taskDescription;
-    private LocalDateTime createdAt;
-    private String taskStatus;
-    private String taskPoint;
+    private Byte taskPointID;
+    private Byte taskStatusID;
     private LocalDateTime taskReviewRequestedAt;
+    private Timestamp taskCreatedAt;
 }
