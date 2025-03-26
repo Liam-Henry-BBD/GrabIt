4d42028b047125d13d23d1f91ed6041b69072f1a
Liam-Henry-BBD <Liam.Henry@bbd.co.za>
2025-03-26 15:59:41 +0200

    connected buttons for leaderboard and task management

--- front-end/src/home/components/project-app.ts
+++ front-end/src/home/components/project-app.ts
@@ -1,39 +1,39 @@
-import { getProject, getProjectTasks } from "../../services/projects.service";
-import { RouterLocation } from "@vaadin/router";
-import { CtLit, html } from "@conectate/ct-lit";
+import { getProject, getProjectTasks } from '../../services/projects.service';
+import { RouterLocation } from '@vaadin/router';
+import { CtLit, html } from '@conectate/ct-lit';
 import { customElement, property, state } from 'lit/decorators.js';
-import { Project, Task, User } from "../../utils/types";
-import { projectAppStyles } from "./project-app.styles";
-import { filterTasks } from "../../utils/app";
-import { getUser } from "../../services/user.service";
-import { completeTask, grabTask, rejectTaskReview, requestTaskReview } from "../../services/task.service";
+import { Project, Task, User } from '../../utils/types';
+import { projectAppStyles } from './project-app.styles';
+import { filterTasks } from '../../utils/app';
+import { getUser } from '../../services/user.service';
+import { completeTask, grabTask, rejectTaskReview, requestTaskReview } from '../../services/task.service';
 
-import "../components/cards/review-card";
+import '../components/cards/review-card';
 
 @customElement('project-app')
 export class ProjectApp extends CtLit {
 	@state()
 	private tasks: Task[] = [];
 
-    @state()
-    private project: Project = {
-        projectID: 0,
-        projectName: "",
-        projectDescription: "",
-        createdAt: "",
-        updatedAt: "",
-        active: false,
-        collaboratorRole: 0,
-    };
+	@state()
+	private project: Project = {
+		projectID: 0,
+		projectName: '',
+		projectDescription: '',
+		createdAt: '',
+		updatedAt: '',
+		active: false,
+		collaboratorRole: 0
+	};
 
-    @state()
-    private currentUser: User = {
-        email: "",
-        fullName: "",
-        picture: "",
-        verified: false,
-        userID: 0,
-    }
+	@state()
+	private currentUser: User = {
+		email: '',
+		fullName: '',
+		picture: '',
+		verified: false,
+		userID: 0
+	};
 
 	@state()
 	private projectID: number = 0;
@@ -54,17 +54,16 @@ export class ProjectApp extends CtLit {
 		this.fetchData();
 	}
 
-    async fetchData() {
-        
-        const response = await getProjectTasks(this.projectID);
-        
-        const project = await getProject(this.projectID);
+	async fetchData() {
+		const response = await getProjectTasks(this.projectID);
 
-        this.currentUser = await getUser() as User;
-       
-        this.project = project;
-        this.tasks = response;
-        this.tasks = filterTasks(this.tasks, this.currentUser.userID);
+		const project = await getProject(this.projectID);
+
+		this.currentUser = (await getUser()) as User;
+
+		this.project = project;
+		this.tasks = response;
+		this.tasks = filterTasks(this.tasks, this.currentUser.userID);
 
 		this.available = this.tasks.filter(task => task.taskStatusID == 1).length;
 		this.grabbed = this.tasks.filter(task => task.taskStatusID == 2).length;
@@ -72,99 +71,93 @@ export class ProjectApp extends CtLit {
 		this.complete = this.tasks.filter(task => task.taskStatusID == 4).length;
 	}
 
-    async handleGrabTask(taskID: number, projectID: number) {
-        const grabbedTask = await grabTask(taskID, projectID) as Task;
-        if (grabbedTask != null) {
-           window.location.reload()
-        }
-    }
+	async handleGrabTask(taskID: number, projectID: number) {
+		const grabbedTask = (await grabTask(taskID, projectID)) as Task;
+		if (grabbedTask != null) {
+			window.location.reload();
+		}
+	}
 
-    async handleRequestReview(taskID: number, _: number) {
-        const requestReview = await requestTaskReview(taskID) as Task;
-        if (requestReview != null) {
-           window.location.reload()
-        }
-    }
+	async handleRequestReview(taskID: number, _: number) {
+		const requestReview = (await requestTaskReview(taskID)) as Task;
+		if (requestReview != null) {
+			window.location.reload();
+		}
+	}
 
-    async handleCancelReview(taskID: number) {
-        const requestReview = await rejectTaskReview(taskID) as Task;
-        if (requestReview != null) {
-           window.location.reload()
-        }
-    }
+	async handleCancelReview(taskID: number) {
+		const requestReview = (await rejectTaskReview(taskID)) as Task;
+		if (requestReview != null) {
+			window.location.reload();
+		}
+	}
 
-    async handleCompleteTask(taskID: number) {
-        const requestReview = await completeTask(taskID) as Task;
-        if (requestReview != null) {
-           window.location.reload()
-        }   
-    }
-
-
-
+	async handleCompleteTask(taskID: number) {
+		const requestReview = (await completeTask(taskID)) as Task;
+		if (requestReview != null) {
+			window.location.reload();
+		}
+	}
 
 	static styles = projectAppStyles;
 
-    render() {
+	render() {
+		return html`<main>
+			<section class="project-head">
+				<article class="project-details">
+					<h1>${this.project ? this.project.projectName : 'Loading...'}</h1>
+					<p class="project-desc">${this.project ? this.project.projectDescription : '...'}</p>
+				</article>
+				<article>
+					<section class="article-buttons">
+						<div>
+							<a href="http://localhost:8000/project/${this.projectID}" class="new-project-body">Manage Tasks</a>
+						</div>
+						<a href="http://localhost:8000/project/${this.projectID}/leaderboard" class="leaderboard-button">Leaderboard</a>
+					</section>
+				</article>
+			</section>
 
-        return html`<main>
+			<section class="tab-content">
+				<section class="columns">
+					<article class="column">
+						<div class="column-space"><span>Available</span> <span class="task-count">${this.available}</span></div>
+						${this.tasks
+							.filter(task => task.taskStatusID == 1)
+							.map(task => {
+								const allowedToGrab = this.project.collaboratorRole != 1;
+								return html`<project-card .visible=${allowedToGrab} .handleTaskAction=${this.handleGrabTask} .task=${task} .action=${'Grab task'}></project-card>`;
+							})}
+					</article>
 
-            <section class="project-head">
-                <article class="project-details" >
-                    <h1>${this.project ? this.project.projectName : 'Loading...'}</h1>
-                    <p class="project-desc">${this.project ? this.project.projectDescription : '...'}</p>
-                </article>
-                <article>
-                <section class="article-buttons">
-                    <div>
-                        <button class="new-project-body">+ New Task</button>
-                        <!-- ${this.currentProjectRole == 1 ? html`<button class="new-collaborator">Update Project</button>` : html``} -->
-                    </div>
-                    <button class="leaderboard-button">Leaderboard</button>
-                </section>
-             </article>
-            </section>
+					<article class="column">
+						<div class="column-space"><span>Grabbed</span> <span class="task-count">${this.grabbed}</span></div>
+						${this.tasks
+							.filter(task => task.taskStatusID == 2)
+							.map(task => {
+								const allowedToGrab = this.project.collaboratorRole != 1 && task.userID == this.currentUser.userID;
+								return html`<project-card .visible=${allowedToGrab} .handleTaskAction=${this.handleRequestReview} .task=${task} .action=${'Request review'}></project-card>`;
+							})}
+					</article>
 
-
-            <section class="tab-content">
-                <section class="columns">
-                    <article class="column">
-                        <div class="column-space"><span>Available</span> <span class="task-count">${this.available}</span></div>
-                        ${
-                            this.tasks.filter(task => task.taskStatusID == 1).map(task => {
-                                const allowedToGrab = (this.project.collaboratorRole != 1); 
-                                return html`<project-card .visible=${allowedToGrab} .handleTaskAction=${this.handleGrabTask} .task=${task} .action=${"Grab task"}></project-card>`;
-                            })
-                        }
-                    </article>
-
-                    <article class="column">
-                        <div class="column-space"><span>Grabbed</span> <span class="task-count">${this.grabbed}</span></div>
-                        ${
-                            this.tasks.filter(task => task.taskStatusID == 2).map(task => {
-                                const allowedToGrab = (this.project.collaboratorRole != 1) && task.userID == this.currentUser.userID;
-                                return html`<project-card .visible=${allowedToGrab} .handleTaskAction=${this.handleRequestReview} .task=${task} .action=${"Request review"}></project-card>`;
-                            })
-                        }
-                    </article>
-
-                    <article class="column">
-                        <div class="column-space"><span>In Review</span> <span class="task-count">${this.review}</span></div>
-                        ${
-                            this.tasks.filter(task => task.taskStatusID == 3).map(task => {
-                                const allowedToComplete = (this.project.collaboratorRole == 1);
-                                const allowedToCancel = task.userID == this.currentUser.userID || this.project.collaboratorRole == 1;
-                                return html`<review-card 
-                                    .handleCancelAction=${this.handleCancelReview} 
-                                    .cancel=${allowedToCancel} 
-                                    .visible=${allowedToComplete} 
-                                    .handleTaskAction=${this.handleCompleteTask} 
-                                    .task=${task} 
-                                    .action=${"Complete"}>
-                                </review-card>`;
-                            })
-                        }
-                    </article>
+					<article class="column">
+						<div class="column-space"><span>In Review</span> <span class="task-count">${this.review}</span></div>
+						${this.tasks
+							.filter(task => task.taskStatusID == 3)
+							.map(task => {
+								const allowedToComplete = this.project.collaboratorRole == 1;
+								const allowedToCancel = task.userID == this.currentUser.userID || this.project.collaboratorRole == 1;
+								return html`<review-card
+									.handleCancelAction=${this.handleCancelReview}
+									.cancel=${allowedToCancel}
+									.visible=${allowedToComplete}
+									.handleTaskAction=${this.handleCompleteTask}
+									.task=${task}
+									.action=${'Complete'}
+								>
+								</review-card>`;
+							})}
+					</article>
 
 					<article class="column">
 						<div class="column-space"><span>Complete</span> <span class="task-count">${this.complete}</span></div>
