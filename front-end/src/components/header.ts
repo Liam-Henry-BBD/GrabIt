7dc88b51d3dd03c222ae79b41f64cc101585e7e1
Tshilidzi Munyai <Tshilidzi.Munyai@bbd.co.za>
2025-03-29 22:27:06 +0200

    feat: add logout link and enhance profile icon styling in HeaderApp component

--- front-end/src/components/header.ts
+++ front-end/src/components/header.ts
@@ -15,18 +15,58 @@ export class HeaderApp extends LitElement {
 
 	render() {
 		return html`
-            <header class="header">
-			<img id="logo" width="140" src="/src/home/home_images/GI_logo-white.png" alt="Logo" @click=${() => window.location.href = 'http://localhost:8000'}>
-				<img id="profile-icon" src=${this.picture ? this.picture : ""} alt="Logo" />
+			<header class="header">
+				<img id="logo" width="140" src="/src/home/home_images/GI_logo-white.png" alt="Logo" @click=${() => window.location.href = 'http://localhost:8000'}>
+				<span class="profile-container">
+					<img id="profile-icon" src=${this.picture ? this.picture : ""} alt="Profile Icon" />
+					<a href="http://localhost:8000" class="logout-link">Logout</a>
+				</span>
 			</header>
-        `;
+		`;
 	}
+	
 
 	static styles = css`
 		.logo {
 			display: flex;
 			justify-content: center;
 			align-items: center;
+		}
+
+
+		.logout-link {
+			background-color: #f9a03f;
+			color: #242423;
+			padding: 0.5rem 0.5rem;
+			text-decoration: none;
+			font-size: 0.9rem;
+			font-weight: bold;
+			border-radius: 0.7rem;
+			margin: 0; 
+			position: absolute; 
+			top: 50%; 
+			right: 0%; 
+			transform: translate(-50%, -50%); 
+		}
+
+		.logout-link:hover {
+			border-radius: 0.7rem;
+			background-color: #242423;
+			color: #f9a03f;
+			text-decoration: underline;
+		}
+
+		@media (max-width: 1024px) {
+			.header {
+				justify-content: center;
+			}
+			#logo {
+				margin: 0 auto;
+			}
+			#profile-icon {
+				position: fixed;
+				right: 0;
+			}
 		}
 		
 		.logo button {
@@ -41,9 +81,11 @@ export class HeaderApp extends LitElement {
 			background-color: inherit;
 			text-decoration: underline;
 		}
+		
 		.logo a {
 			margin-top: 7px;
 		}
+		
 		.header {
 			display: flex;
 			justify-content: space-between;
@@ -61,10 +103,10 @@ export class HeaderApp extends LitElement {
 		}
 
 		#profile-icon {
-			margin-right: 3rem;
+			margin-right: 10rem;
 			margin-left: 3rem;
-			height: 2.5rem;
-			width: 2.5rem;
+			height: 3rem;
+			width: 3rem;
 			border-radius: 50%;
 		}
 		.header input {
